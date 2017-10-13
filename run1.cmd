8c53db98ddbc2b1c70a81fd8fbbb170190cf56cc
Calvin Neo <calvinneo1995@gmail.com>
2017-10-13 11:16:08 +0800

    initial
    	modified:   .gitignore
    	new file:   config
    	new file:   kvstore.iml
    	new file:   pom.xml
    	new file:   run1.cmd
    	new file:   run2.cmd
    	new file:   run3.cmd
    	new file:   src/main/java/KVProcessor.java
    	new file:   src/main/java/META-INF/MANIFEST.MF

---
+++ run1.cmd
@@ -0,0 +1,1 @@
+java -classpath ./out/artifacts/kvstore_jar/kvstore.jar cn.helium.kvstore.main.Main --restful-server-port 8500 --kvpod-id 0 --processor-class KVProcessor --hdfs-port 9000 --hdfs-host "127.0.0.1" --kvpods-hosts "localhost,localhost,localhost" --kvpods-rpc-ports "8090,8091,8092"
---
+++ run3.cmd
@@ -0,0 +1,1 @@
+java -classpath ./out/artifacts/kvstore_jar/kvstore.jar cn.helium.kvstore.main.Main --restful-server-port 8502 --kvpod-id 2 --processor-class KVProcessor --hdfs-port 9000 --hdfs-host "127.0.0.1" --kvpods-hosts "localhost,localhost,localhost" --kvpods-rpc-ports "8090,8091,8092"
